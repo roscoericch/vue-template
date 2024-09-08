799a80b7edc2d7967f3c778b7d30fefcfc8a5812
sikiru razak <sikirurazak1@gmail.com>
2024-09-08 10:32:35 +0100

    update tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import {useCounterStore} from "@/stores/counter.ts";
+  import {useCounterStore} from "@/stores/counter";
   const countStore = useCounterStore();
 </script>
 
@@ -7,11 +7,13 @@
   <main>
     <!-- <TheWelcome /> -->
      <header>Home</header>
-     <span class="">
+     <span class="flex gap-1">
       <p v-if="countStore.count > 0">count:</p>
       <p>{{ countStore.count }}</p>
      </span>
-     <button @click="countStore.increment">increment</button>
-     <button @click="countStore.decrement">decrement</button>
+     <div class="flex gap-1">
+      <button class="bg-white text-black rounded-[6px] p-[0.1rem]" @click="countStore.increment">increment</button>
+      <button class="bg-white text-black rounded-[6px] p-[0.1rem]" @click="countStore.decrement">decrement</button>
+    </div>
   </main>
 </template>
